0535636b97b661b95bc5ee03a47fa7c3c8986087
DonizeteVida <donizetevida1998@outlook.com>
2019-02-02 03:31:40 -0200

    Alguns Dao consertados

--- src/main/java/uDao/VeiculoDao.java
+++ src/main/java/uDao/VeiculoDao.java
@@ -1,6 +1,5 @@
 package uDao;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,19 +9,19 @@ import entities.Veiculo;
 
 public class VeiculoDao extends BaseDao {
 
-	private Connection con;
-
 	public VeiculoDao() {
 		super();
-		con = getConnection();
 	}
 
 	public Veiculo buscaVeiculo(Integer id_veiculo) throws SQLException {
 
-		String sql = "SELECT * FROM veiculo WHERE id_veiculo =?";
-		PreparedStatement ps = con.prepareStatement(sql);
+		String sql = "SELECT * FROM veiculos WHERE id_veiculo = ?;";
+
+		PreparedStatement ps = getConnection().prepareStatement(sql);
 		ps.setInt(1, id_veiculo);
+
 		ResultSet rs = ps.executeQuery();
+
 		Veiculo v = null;
 
 		if (rs.next()) {
--- src/main/java/uDao/ViagemDao.java
+++ src/main/java/uDao/ViagemDao.java
@@ -1,15 +1,10 @@
 package uDao;
 
-import java.sql.Connection;
-
 import JDBC.BaseDao;
 
 public class ViagemDao extends BaseDao {
 
-	private Connection con;
-
 	public ViagemDao() {
 		super();
-		con = getConnection();
 	}
 }
