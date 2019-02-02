bbcbc3aa2542f2dc959b15cf957feb91a446d445
DonizeteVida <donizetevida1998@outlook.com>
2019-02-02 03:12:35 -0200

    FuncionarioDao completa

--- src/main/java/entities/Funcionario.java
+++ src/main/java/entities/Funcionario.java
@@ -5,8 +5,13 @@ public class Funcionario {
 	private Integer id_funcionario;
 	private String nome;
 	private String cpf;
-	private Integer id_cargo;
-	private Integer id_filial;
+	private ClasseGenerica cargo;
+	private ClasseGenerica filial;
+
+	public Funcionario() {
+		cargo = new ClasseGenerica();
+		filial = new ClasseGenerica();
+	}
 
 	public Integer getId_funcionario() {
 		return id_funcionario;
@@ -32,21 +37,20 @@ public class Funcionario {
 		this.cpf = cpf;
 	}
 
-	public Integer getId_cargo() {
-		return id_cargo;
+	public ClasseGenerica getCargo() {
+		return cargo;
 	}
 
-	public void setId_cargo(Integer id_cargo) {
-		this.id_cargo = id_cargo;
-
+	public void setCargo(ClasseGenerica cargo) {
+		this.cargo = cargo;
 	}
 
-	public Integer getId_filial() {
-		return id_filial;
+	public ClasseGenerica getFilial() {
+		return filial;
 	}
 
-	public void setId_filial(Integer id_filial) {
-		this.id_filial = id_filial;
+	public void setFilial(ClasseGenerica filial) {
+		this.filial = filial;
 	}
 
 }
