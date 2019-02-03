1b9670453f532df7af569d6ada8bc51098b03dee
Ednilton Junior <jrjunior_cg@live.com>
2019-02-03 00:59:23 -0300

    Adicionada novas classes
    *ViagemConteudo
    *TipoCNH
    *Filial
    *Cargo

---
+++ src/main/java/entities/TipoCNH.java
@@ -0,0 +1,23 @@
+package entities;
+
+public class TipoCNH {
+	
+	private Integer id_tipo_cnh;
+	private String nome;
+	
+	public Integer getId_tipo_cnh() {
+		return id_tipo_cnh;
+	}
+	public void setId_tipo_cnh(Integer id_tipo_cnh) {
+		this.id_tipo_cnh = id_tipo_cnh;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
---
+++ src/main/java/entities/ViagemConteudo.java
@@ -0,0 +1,23 @@
+package entities;
+
+public class ViagemConteudo {
+	
+	private Integer id_viagem;
+	private Integer id_funcionario;
+	
+	public Integer getId_viagem() {
+		return id_viagem;
+	}
+	public void setId_viagem(Integer id_viagem) {
+		this.id_viagem = id_viagem;
+	}
+	public Integer getId_funcionario() {
+		return id_funcionario;
+	}
+	public void setId_funcionario(Integer id_funcionario) {
+		this.id_funcionario = id_funcionario;
+	}
+	
+	
+
+}
